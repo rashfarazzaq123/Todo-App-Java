1b62615c0ad84c0767cbe51452e6f86df22e693a
rashfarazzaq123 <rashfarazzaq@gmail.com>
2021-07-02 02:47:30 +0530

    Initial commit

---
+++ src/main/java/lk/rashfarazzaq/todo/TodoApplication.java
@@ -0,0 +1,14 @@
+package lk.rashfarazzaq.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+}
