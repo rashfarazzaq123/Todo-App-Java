cf67f520b9f0e6d469b6f3cbc10ce3a2e1525959
rashfarazzaq123 <rashfarazzaq@gmail.com>
2021-07-02 03:27:43 +0530

    Setting Timezone

--- src/main/java/lk/rashfarazzaq/todo/TodoApplication.java
+++ src/main/java/lk/rashfarazzaq/todo/TodoApplication.java
@@ -1,5 +1,9 @@
 package lk.rashfarazzaq.todo;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,5 +14,11 @@ public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Colombo"));
 	}
 }
